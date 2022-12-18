872f48c114a39388d399dbaa9fbfda41c6f15796
gnkz <dellarosciagiorgio@gmail.com>
2022-12-18 20:39:58 +0100

    c

---
+++ ID7.java
@@ -0,0 +1,26 @@
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+// What is the 10 001st prime number?
+
+public class ID7 {
+    public static boolean isPrime (int x) {
+        boolean prime = true;
+        for (int i = 2; i < x; ++i) {
+            if (x % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
+    public static void main (String[] args) {
+        int c = 0, x;
+        for (x = 2; c != 10001; ++x) {
+            boolean prime = isPrime(x);
+            if (prime) {
+                ++c;
+            }
+        }
+        System.out.println("the 10 001st prime number is: " + (x - 1));
+    }
+}
